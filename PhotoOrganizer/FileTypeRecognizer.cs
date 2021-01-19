3295e7166bb675237c3d12ca51a728c71ee7a2dc
Ryan Gregg <rgregg@users.noreply.github.com>
2021-01-18 22:34:05 -0800

    Add more formats.

--- PhotoOrganizer/FileTypeRecognizer.cs
+++ PhotoOrganizer/FileTypeRecognizer.cs
@@ -9,6 +9,8 @@ namespace PhotoOrganizer
 {
     public class FileTypeRecognizer
     {
+        // Useful list of file signatures here: https://en.wikipedia.org/wiki/List_of_file_signatures
+
         private static List<FormatMagicData> Formats = new List<FormatMagicData>()
         {
             new FormatMagicData(MediaType.Image, FileBinaryFormat.Jpeg, ".jpg", 
@@ -17,13 +19,21 @@ namespace PhotoOrganizer
                 new byte?[] { 0xFF, 0xD8, 0xFF, 0xE1, null, null, 0x45, 0x78, 0x69, 0x66, 0x00 },
                 new byte?[] { 0xFF, 0xD8, 0xFF, 0xEE }),
             new FormatMagicData(MediaType.Image, FileBinaryFormat.Png, ".png", 
-                new byte?[] { 137, 80, 78, 71, 13, 10, 26, 10 }),
+                new byte?[] { 0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A }),
             new FormatMagicData(MediaType.Image, FileBinaryFormat.Heic, ".heic", 
                 new byte?[] { null, null, null, null, 0x66, 0x74, 0x79, 0x70, 0x68, 0x65, 0x69, 0x63, 0x00 }),
             new FormatMagicData(MediaType.Video, FileBinaryFormat.QuickTime, ".mov",
                 new byte?[] { 0x00, 0x00, 0x00, 0x14, 0x66, 0x74, 0x79, 0x70, 0x71, 0x74, 0x20, 0x20, 0x00, 0x00, 0x00, 0x00, 0x71, 0x74, 0x20, 0x20 }),
             new FormatMagicData(MediaType.Image, FileBinaryFormat.CanonRawCr2, ".cr2", 
-                new byte?[] { 0x49, 0x49, 0x2A, 0x00, 0x10, 0x00, 0x00, 0x00, 0x43, 0x52, 0x02, 0x00 })
+                new byte?[] { 0x49, 0x49, 0x2A, 0x00, 0x10, 0x00, 0x00, 0x00, 0x43, 0x52, 0x02, 0x00 }),
+            new FormatMagicData(MediaType.Image, FileBinaryFormat.Gif, ".gif",
+                new byte?[] { 0x47, 0x49, 0x46, 0x38, 0x37, 0x61},
+                new byte?[] { 0x47, 0x49, 0x46, 0x38, 0x39, 0x61}),
+            new FormatMagicData(MediaType.Video, FileBinaryFormat.Avi, ".avi", 
+                new byte?[] { 0x52, 0x49, 0x46, 0x46, null, null, null, null, 0x41, 0x56, 0x49, 0x20 }),
+            new FormatMagicData(MediaType.Video, FileBinaryFormat.Mp4, ".mp4", 
+                new byte?[] { 0x00, 0x00, 0x00, 0x18, 0x66, 0x74, 0x79, 0x70, 0x69, 0x73, 0x6F, 0x6D })
+
         };
 
         static int S_MaxBytes = -1;
@@ -122,7 +132,6 @@ namespace PhotoOrganizer
         CanonRawCr2,
         Gif,
         Avi,
-        Mov,
         Mp4,
         Heic,
         QuickTime
